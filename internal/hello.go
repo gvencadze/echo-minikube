38e2ee08ac84097ae4fa609038ae09b7a15fcf53
K.Gvencadze <k.gvencadze@icloud.com>
2022-02-12 19:29:47 +0300

    feat(internal/hello): add query parameter validate

--- internal/hello.go
+++ internal/hello.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,12 @@ import (
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode("name parameter is required")
+		return
+	}
 
 	if _, err := fmt.Fprintf(w, "Hello from server in minikube, %s", name); err != nil {
 		log.Println(err)
